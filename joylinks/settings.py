174846b31f5d214dcb451a73268ca32070704290
FirePlay <dodajoy077@mail.ru>
2025-12-25 17:42:22 +0500

    db switched to postgresql

--- joylinks/settings.py
+++ joylinks/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'joylinks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'joylinks_db',
-#         'USER': 'postgres',
-#         'PASSWORD': '125232',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'joylinks_db',
+        'USER': 'postgres',
+        'PASSWORD': '125232',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
